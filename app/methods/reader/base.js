b20d94b8748a996e4c9dc00f5e926571d802c76a
memset0 <memset0@outlook.com>
2020-04-14 20:44:10 +0800

    feat(reader): 显示作者等信息

--- app/methods/reader/base.js
+++ app/methods/reader/base.js
@@ -6,13 +6,19 @@ class BaseReader {
 		console.log(this.data, $e)
 		$e.append(`
 			<div class="lavandula-container">
-				<div class="lavandula-title">
-					${this.data.title}
-				</div>
-				<div class="lavandula-subtitle">
-				</div>
-				<div class="lavandula-typo">
-					${this.data.content}
+				<div class="lavandula-content">
+					<div class="lavandula-title">
+						${this.data.title}
+					</div>
+					<div class="lavandula-subtitle">
+						<a class="lavandula-chip" href="${this.data.author_link || '#'}">
+							<span class="lavandula-chip-icon"><i class="lavandula-icon material-icons">person</i></span>
+							<span class="lavandula-chip-title">${this.data.author}</span>
+						</a>
+					</div>
+					<div class="lavandula-typo">
+						${this.data.content}
+					</div>
 				</div>
 			</div>
 		`)
