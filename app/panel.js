4bdf6dff7bed4ffa87cd2154222f966f7a70d68d
memset0 <memset0@outlook.com>
2020-04-14 11:37:08 +0800

    style: 整理代码

--- app/panel.js
+++ app/panel.js
@@ -5,28 +5,23 @@ class Panel {
 		this.ele.toggleClass('lavandula-hide')
 	}
 	create() {
-		this.ele = createElement(
-			'div',
-			{
-				id: 'lavandula-panel',
-				class: 'lavandula-hide'
-			}
-		)
-		this.infoEle = $(`
+		this.ele = createElement('div', {
+			id: 'lavandula-panel',
+			class: 'lavandula-hide'
+		})
+		this.ele.append($(`
 			<div id="lavandula-panel-info" class="lavandula-card">
 				<div class="lavandula-card-primary">
 					<div class="lavandula-card-primary-title">${$('title').get(0).innerHTML}</div>
 				</div>
 			</div>
-		`)
-		this.ele.append(this.infoEle)
+		`))
 		return this.ele
 	}
 	remove() {
 		$('lavandula-panel').remove()
 	}
 	constructor() {
-		
 	}
 }
 
