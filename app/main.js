bbcea74da1a2794a03a2785b9080d27b78f82682
memset0 <memset0@outlook.com>
2020-04-15 16:37:12 +0800

    feat: 引入GM API

--- app/main.js
+++ app/main.js
@@ -1,11 +1,15 @@
 import css from './style/main.less'
 
 $(document).ready(function () {
-	window.lavandula = {
+	unsafeWindow.lavandula = {
 		tools: new (require('./tools.js'))(),
 		panel: new (require('./panel.js'))(),
 		reader: new (require('./reader.js'))(),
 		button: new (require('./button.js'))(),
+		GM_getValue: GM.getValue,
+		GM_setValue: GM.setValue,
+		GM_listValues: GM.listValues,
+		GM_deleteValue: GM.deleteValue,
 	}
 	if (lavandula.reader.loaded) {
 		$('body').append(lavandula.reader.create())
