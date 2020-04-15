2bb1049d10d7e51686774f2c75a9a3c2bd996a9b
memset0 <memset0@outlook.com>
2020-04-15 17:12:26 +0800

    feat: 引入GM API

--- app/main.js
+++ app/main.js
@@ -1,22 +1,29 @@
 import css from './style/main.less'
 
 $(document).ready(function () {
+	const mode = 'development'
 	unsafeWindow.lavandula = {
 		tools: new (require('./tools.js'))(),
 		panel: new (require('./panel.js'))(),
 		reader: new (require('./reader.js'))(),
 		button: new (require('./button.js'))(),
-		GM_getValue: GM.getValue,
-		GM_setValue: GM.setValue,
-		GM_listValues: GM.listValues,
-		GM_deleteValue: GM.deleteValue,
+	}
+	if (mode == 'development') {
+		unsafeWindow.lavandula = Object.assign(unsafeWindow.lavandula, {
+			GM_getValue: GM.getValue,
+			GM_setValue: GM.setValue,
+			GM_listValues: GM.listValues,
+			GM_deleteValue: GM.deleteValue,
+		})
 	}
 	if (lavandula.reader.loaded) {
 		$('body').append(lavandula.reader.create())
 	}
 	$('body').append(lavandula.panel.create())
 	$('body').append(lavandula.button.create())
-	if (lavandula.reader.loaded) {
-		$('#lavandula-toggle-button').click()
+	if (mode == 'development') {
+		if (lavandula.reader.loaded) {
+			$('#lavandula-toggle-button').click()
+		}
 	}
 })
